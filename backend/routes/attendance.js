2ab4bc8184af9a5bca17ad651f7df68b5d7ae317
LuanBog <mynameiscutepanda@gmail.com>
2023-05-28 16:06:19 +0800

    Added time out

--- backend/routes/attendance.js
+++ backend/routes/attendance.js
@@ -8,18 +8,40 @@ router.route('/in/:id').get((req, res) => {
             const time = date.getDay() + "/" + date.getMonth()  + "/" + date.getFullYear() + " @ "  + date.getHours() + ":"  + date.getMinutes() + ":" + date.getSeconds();
 
             const data = {
-                "time-in": time,
-                "time-out": ''
+                "timeIn": time,
+                "timeOut": ''
             };
-
-            console.log(data);
 
             student.attendance.push(data);  
 
             student.save()
             .then(() => { 
-                console.log(`[Student] ${student.fullName} got updated`);
-                res.json(student);
+                console.log(`[Attendance] ${student.fullName} just timed in at ${time}`);
+                res.json(data);
+            })
+            .catch(err => res.status(400).json(`Error ${err}`));
+        });
+});
+
+router.route('/out/:id').get((req, res) => {
+    Student.findById(req.params.id)
+        .then(student => {
+            const date = new Date();
+            const time = date.getDay() + "/" + date.getMonth()  + "/" + date.getFullYear() + " @ "  + date.getHours() + ":"  + date.getMinutes() + ":" + date.getSeconds();
+
+            const latestAttendance = student.attendance[student.attendance.length - 1];
+
+            const data = {
+                "timeIn": latestAttendance.timeIn,
+                "timeOut": time
+            };
+
+            student.attendance[student.attendance.length - 1] = data;
+
+            student.save()
+            .then(() => { 
+                console.log(`[Attendance] ${student.fullName} just timed out at ${time}`);
+                res.json(data);
             })
             .catch(err => res.status(400).json(`Error ${err}`));
         });
